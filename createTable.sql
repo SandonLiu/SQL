d42dcc37c6e38817eff719bc60fb592880c2d45b
SandonLiu <liushengxun888@gmail.com>
2022-06-25 11:23:15 +0900

    feat($sql):添加新SQL

--- createTable.sql
+++ createTable.sql
@@ -1,4 +1,4 @@
---建立雇员表
+--建立雇员�?
 CREATE TABLE `employee` (
   `empId` char(10) NOT NULL,
   `empName` varchar(45) NOT NULL,
@@ -6,5 +6,5 @@ CREATE TABLE `employee` (
   `updateDateTime` datetime DEFAULT NULL,
   `updateNumber` int DEFAULT NULL,
   PRIMARY KEY (`empId`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='员工表'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='员工�?
 
